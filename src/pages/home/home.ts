560c6b4fa55bd5e9ba4108d7cef0ca4c1bbc43e6
Adrián Mujica González <mujicagadrian@gmail.com>
2018-02-24 13:30:23 +0100

    Show marks from array

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,11 +9,27 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   title: string = 'My first AGM project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
 
-  constructor(public navCtrl: NavController) {
-
+  locations: any = [
+  {
+    lat: 51.378418,
+    lng: 7.409007
+  },
+  {
+    lat: 51.578418,
+    lng: 7.709007
+  },
+  {
+    lat: 51.478418,
+    lng: 7.609007
   }
+];
+
+lat: number = 51.678418;
+lng: number = 7.809007;
+
+constructor(public navCtrl: NavController) {
+
+}
 
 }
